afece9bb9f28a1ad548545e7fe9125e9bf6b1b39
Tindra Admin <motechgroupkenya@gmail.com>
2026-07-28 17:49:04 +0300

    Fix mobile menu to be strictly closed by default on page load and toggle smoothly on click

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -195,6 +195,7 @@
 
         <!-- Mobile Drawer Navigation Overlay -->
         <div x-show="mobileMenu" 
+             :style="mobileMenu ? 'background-color: #0b0e18; display: flex;' : 'background-color: #0b0e18; display: none !important;'"
              x-transition:enter="transition ease-out duration-200"
              x-transition:enter-start="opacity-0 -translate-y-2"
              x-transition:enter-end="opacity-100 translate-y-0"
@@ -202,8 +203,8 @@
              x-transition:leave-start="opacity-100 translate-y-0"
              x-transition:leave-end="opacity-0 -translate-y-2"
              @click.away="mobileMenu = false"
-             class="mobile-drawer-menu md:hidden fixed left-0 right-0 top-[72px] sm:top-[80px] bottom-0 z-[999] w-full text-white overflow-y-auto px-5 py-6 flex flex-col justify-between shadow-2xl border-t border-slate-800" 
-             style="background-color: #0b0e18;"
+             class="mobile-drawer-menu md:hidden fixed left-0 right-0 top-[72px] sm:top-[80px] bottom-0 z-[999] w-full text-white overflow-y-auto px-5 py-6 flex-col justify-between shadow-2xl border-t border-slate-800" 
+             style="background-color: #0b0e18; display: none !important;"
              x-cloak>
             
             <div class="space-y-6">
